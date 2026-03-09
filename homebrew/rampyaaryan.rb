215ee9799ed8a91f4cbbb990e8b3328154374382
Rampyaaryans <196038338+Rampyaaryans@users.noreply.github.com>
2026-03-09 10:18:36 +0530

    Add package manager manifests

---
+++ homebrew/rampyaaryan.rb
@@ -0,0 +1,19 @@
+class Rampyaaryan < Formula
+  desc "India's First Hinglish Programming Language"
+  homepage "https://rampyaaryans.github.io/rampyaaryan/"
+  url "https://github.com/Rampyaaryans/rampyaaryan/archive/refs/tags/v3.4.0.tar.gz"
+  sha256 "REPLACE_WITH_ACTUAL_SHA256"
+  license "LicenseRef-Rampyaaryan"
+
+  depends_on "gcc" => :build
+
+  def install
+    system "make", "release"
+    bin.install "bin/rampyaaryan"
+  end
+
+  test do
+    (testpath/"hello.ram").write('likho "Namaste Duniya!"')
+    assert_match "Namaste Duniya!", shell_output("#{bin}/rampyaaryan #{testpath}/hello.ram")
+  end
+end
